b70719dab47bde038e3f54ba20a07cf1626999fe
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 <4m1d0n@gmail.com>
2025-02-03 14:54:44 +0100

    second commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,15 +15,17 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/4m1d0n.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: '4m1d0n', // Usually your GitHub org/user name.
+  projectName: '4m1d0n.github.io', // Usually your repo name.
+
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
